32addba503a6468ad61389fc59e6d333ddacb13b
Angel Diaz <warydiaz@hotmail.com>
2023-07-22 13:47:56 +0200

    Redirect to login if the user is not authenticated

--- frontend/leave-request-manager/app/page.tsx
+++ frontend/leave-request-manager/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { redirect } from "next/navigation";
+import { cookies } from 'next/headers';
 
-export default function Home() {
-  return (
-   <main>
-    Leave Request
-   </main>
-  )
-}
+  export default async function Home() {
+    const supabase = createServerComponentClient({cookies});
+    const { data } = await supabase.auth.getUser();
+
+    if(!data.user){
+      redirect("/login")
+    }
+    return <main>Leave Request</main>;
+  }

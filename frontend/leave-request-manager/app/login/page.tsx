09926d5a0afad12c4d71609e8b9d8dced991be26
Angel Diaz <warydiaz@hotmail.com>
2023-07-22 19:47:32 +0200

    Creating login page

--- frontend/leave-request-manager/app/login/page.tsx
+++ frontend/leave-request-manager/app/login/page.tsx
@@ -1,7 +1,58 @@
-export default async function Page() {
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { FormEventHandler, useState } from "react";
+
+export default function Page() {
+  const supabase = createClientComponentClient();
+  const [errMsg, setErrMsg] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const singIn = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      setErrMsg(error.toString());
+    } else {
+      router.replace("/");
+    }
+  };
+
+  const formSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    singIn();
+  };
+
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold">Loging Page</h1>
+      <h1 className="text-2xl font-bold">Login Page</h1>
+      <form onSubmit={formSubmit} className="flex flex-col border p-4 rounded">
+        <label className="flex flex-col gap-2 mb-2">
+          <span>Email:</span>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className="flex flex-col gap-2 mb-2">
+          <span>Password:</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button className="bg-stone-200 py-1 px-3 rounded" onClick={singIn}>
+          Sigin
+        </button>
+      </form>
+      {errMsg && <div className="text-red-600 font-bold">{errMsg}</div>}
     </main>
   );
 }

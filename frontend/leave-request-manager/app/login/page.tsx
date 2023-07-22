6f60c868629a635c8c8723ddf74cb52682119a2c
Angel Diaz <warydiaz@hotmail.com>
2023-07-22 14:56:41 +0200

    adding route login

---
+++ frontend/leave-request-manager/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <main className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold">Loging Page</h1>
+    </main>
+  );
+}

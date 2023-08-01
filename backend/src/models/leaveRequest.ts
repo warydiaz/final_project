4c857956e50f9b7be6850815ddf7022c9e0f7e67
Angel Diaz <warydiaz@hotmail.com>
2023-08-01 20:14:43 +0200

    Adding leave request to models

---
+++ backend/src/models/leaveRequest.ts
@@ -0,0 +1,39 @@
+export class LeaveRequest {
+  public id: number | undefined;
+  public employeeId: string;
+  public startDate: Date;
+  public endtDate: Date;
+  public hours_off_requeted: number;
+  public status: string;
+
+  constructor(
+    employeeId: string,
+    startDate: Date,
+    endtDate: Date,
+    hours_off_requeted: number,
+    status: string
+  ) {
+    this.id = undefined;
+    this.employeeId = employeeId;
+    this.startDate = startDate;
+    this.endtDate = endtDate;
+    this.hours_off_requeted = hours_off_requeted;
+    this.status = status;
+  }
+
+  public setId(id: number): void {
+    this.id = id;
+  }
+
+  public getObjEmployee(): any{
+    const aLeaveRequest = {
+      "employeeId": this.employeeId,
+      "startDate": this.startDate,
+      "endtDate": this.endtDate,
+      "hours_off_requeted": this.hours_off_requeted,
+      "status": this.status
+    };
+    return aLeaveRequest;
+  }
+
+}

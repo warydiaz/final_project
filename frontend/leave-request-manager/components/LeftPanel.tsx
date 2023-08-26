0c68ad08c9364bb04fdcce343104d143e4d64553
Angel Diaz <warydiaz@hotmail.com>
2023-08-26 10:47:21 +0200

    Deleting text

--- frontend/leave-request-manager/components/LeftPanel.tsx
+++ frontend/leave-request-manager/components/LeftPanel.tsx
@@ -6,7 +6,6 @@ import LeaveRequestLeftPanel from "./leaveRequestLeftPanel"
 export default function LeftPanel() {
   return (
     <div className="flex-row px-3 py-4 border-b border-r w-80">
-      <div className="font-bold">Left Menu</div>
       <SectorLeftPanel />
       <EmployeeLeftPanel />
       <HolidaysTypeLeftPanel />

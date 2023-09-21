329d0a865af2b849d5652b8f78fa77d198ca3bf7
Angel Diaz <warydiaz@hotmail.com>
2023-09-21 11:23:10 +0200

    Deleting not used imports

--- frontend/leave-request-manager/components/AddLeaveRequest.tsx
+++ frontend/leave-request-manager/components/AddLeaveRequest.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FormEventHandler, useState, useEffect } from "react";
+import React, { FormEventHandler, useState } from "react";
 import { AddALeaveRequest } from "../services/api";
 
 interface AddLeaveRequestProps {
